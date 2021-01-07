17b911a2f339ef82427f00b8c87e9040f7dfc6ee
June YAMAMOTO <june@yambal.net>
2021-01-07 22:04:45 +0900

    FeedParser

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var express = require("express");
 var app = express();
+
+var FeedParser = require('feedparser');
+
 const PORT = process.env.PORT || 5000
 
 
@@ -22,5 +25,50 @@ var photoList = [
 ]
 
 app.get("/api", function(req, res, next){
-    res.json({url: req.query.url})
+    // res.json({url: req.query.url})
+    return parseFeed('https://www.nhk.or.jp/r-news/podcast/nhkradionews.xml')
+        .then((feeds) => {
+            const enc = feeds[0]["rss:enclosure"] || feeds[0].enclosure
+            const encUrl = enc["@"].url
+            res.json({encUrl: encUrl})
+        })
 });
+
+//
+const parseFeed = (url) => {
+    console.error(url);
+   return new Promise((resolve, reject) => {
+    var req = fetch(url)
+    var feedparser = new FeedParser();
+  
+    let feeds = []
+    req.then(function (res) {
+      if (res.status !== 200) {
+        throw new Error('Bad status code');
+      }
+      else {
+        res.body.pipe(feedparser);
+      }
+    }, function (err) {
+  
+    });
+  
+    feedparser.on('error', function (error) {
+  
+    });
+  
+    feedparser.on('readable', function () {
+      var stream = feedparser;
+      var item;
+     
+      while (item = stream.read()) {
+        feeds.push(item)
+      }
+    });
+  
+    feedparser.on('end', function() {
+      console.log('---------- parser end ----------');
+      resolve(feeds)
+    });
+   })
+}
