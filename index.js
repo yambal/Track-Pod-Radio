4c60f1e47ed3e60fd30a093a63a1ed3d309104e0
yambal <jy@yambal.net>
2021-01-07 16:55:05 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+var express = require("express");
+var app = express();
+
+var server = app.listen(3000, function(){
+    console.log("Node.js is listening to PORT:" + server.address().port);
+});
+
+var photoList = [
+    {
+        id: "001",
+        name: "photo001.jpg",
+        type: "jpg",
+        dataUrl: "http://localhost:3000/data/photo001.jpg"
+    },{
+        id: "002",
+        name: "photo002.jpg",
+        type: "jpg",
+        dataUrl: "http://localhost:3000/data/photo002.jpg"
+    }
+]
+
+app.get("/api/photo/list", function(req, res, next){
+    res.json(photoList);
+});
